5f507a294ddbb1f63a660cd09b8c81cc74b952e2
Bruno Cardoso <bruno.cardoso.2203@gmail.com>
2024-04-12 14:36:07 -0300

    added menu

---
+++ tiny_swords/management/menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _ready()-> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		button.pressed.connect(on_button_pressed.bind(button.name))
+
+func on_button_pressed(button_name: String)-> void:
+	match button_name:
+		"NewGame":
+			get_tree().change_scene_to_file("res://tiny_swords/management/level.tscn")
+		"Quit":
+			get_tree().quit()
+				
+func _process(delta):
+	pass
